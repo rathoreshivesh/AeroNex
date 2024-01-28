15594d3ec0335913c00846e181af4e70118d2165
user2 <user2@Shiveshs-MacBook-Pro.local>
2024-01-28 18:31:29 +0530

    initial commit

---
+++ AeroNex/AeroNexApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroNexApp.swift
+//  AeroNex
+//
+//  Created by user2 on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AeroNexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
